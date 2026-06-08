9523e81459be42a17c6f25d4fba63d22122c4ad5
Daniele Zotta <danielezotta@gmail.com>
2026-06-08 19:19:23 +0200

    seo: add dynamic events and gallery albums to sitemap with lastmod

--- src/resources/views/sitemap.blade.php
+++ src/resources/views/sitemap.blade.php
@@ -2,22 +2,56 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   @php
     $namedRoutes = [
-      'home',
-      'storia', 'abito-tradizionale', 'organico', 'maestro',
-      'eventi', 'repertorio', 'galleria', 'contatti',
-      'privacy-policy', 'corsi-di-musica', 'italia-gira-banda', 'chi-siamo',
+      'home'       => 1.0,
+      'chi-siamo'  => 0.8,
+      'storia'     => 0.8,
+      'organico'   => 0.8,
+      'maestro'    => 0.8,
+      'repertorio' => 0.8,
+      'abito-tradizionale' => 0.7,
+      'italia-gira-banda'  => 0.7,
+      'corsi-di-musica'    => 0.7,
+      'privacy-policy'     => 0.3,
+      'eventi'     => 0.9,
+      'galleria'   => 0.9,
+      'contatti'   => 0.7,
     ];
   @endphp
 
   @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-    @foreach($namedRoutes as $routeName)
+    @foreach($namedRoutes as $routeName => $priority)
       @if(Route::has($routeName))
         <url>
           <loc>{{ LaravelLocalization::localizeURL(route($routeName), $localeCode) }}</loc>
           <changefreq>weekly</changefreq>
-          <priority>{{ $routeName === 'home' ? '1.0' : '0.7' }}</priority>
+          <priority>{{ $priority }}</priority>
         </url>
       @endif
+    @endforeach
+
+    @php
+      $events = \App\Models\Event::public()->get();
+      $galleries = \App\Models\GalleryAlbum::where('is_published', 1)->get();
+    @endphp
+
+    @foreach($events as $event)
+      @continue(!Route::has('eventi.show'))
+      <url>
+        <loc>{{ LaravelLocalization::localizeURL(route('eventi.show', $event->slug), $localeCode) }}</loc>
+        <lastmod>{{ $event->updated_at->toW3cString() }}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+      </url>
+    @endforeach
+
+    @foreach($galleries as $album)
+      @continue(!Route::has('galleria.album'))
+      <url>
+        <loc>{{ LaravelLocalization::localizeURL(route('galleria.album', $album->slug), $localeCode) }}</loc>
+        <lastmod>{{ $album->updated_at->toW3cString() }}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+      </url>
     @endforeach
   @endforeach
 </urlset>
